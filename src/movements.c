bcb19da5d982fa9429712a29df2e4e942b0b3c14
Baptiste MENANT <bmenant@student.42.fr>
2019-08-03 16:41:59 +0200

    make push

--- src/movements.c
+++ src/movements.c
@@ -20,20 +20,24 @@ static void	up_and_down(t_data *data, t_player *player, int key)
 	if (key == ARROW_UP)
 	{
 		if (data->map.map[(int)(player->pos[X] + player->dir[X] *
-					player->move_speed)][(int)(player->pos[Y])] == 0)
-			player->pos[X] += player->dir[X] * player->move_speed;
+					player->move_speed)][(int)(player->pos[Y])] != 1)
+			player->pos[X] += player->dir[X] * player->move_speed *
+			player->sprint;
 		if (data->map.map[(int)player->pos[X]][(int)(player->pos[Y] +
-					player->dir[Y] * player->move_speed)] == 0)
-			player->pos[Y] += player->dir[Y] * player->move_speed;
+					player->dir[Y] * player->move_speed)] != 1)
+			player->pos[Y] += player->dir[Y] * player->move_speed *
+			player->sprint;
 	}
 	if (key == ARROW_DOWN)
 	{
 		if (data->map.map[(int)(player->pos[X] - player->dir[X] *
-					player->move_speed)][(int)player->pos[Y]] == 0)
-			player->pos[X] -= player->dir[X] * player->move_speed;
+					player->move_speed)][(int)player->pos[Y]] != 1)
+			player->pos[X] -= player->dir[X] * player->move_speed *
+			player->sprint;
 		if (data->map.map[(int)player->pos[X]][(int)(player->pos[Y] -
-					player->dir[Y] * player->move_speed)] == 0)
-			player->pos[Y] -= player->dir[Y] * player->move_speed;
+					player->dir[Y] * player->move_speed)] != 1)
+			player->pos[Y] -= player->dir[Y] * player->move_speed *
+			player->sprint;
 	}
 }
 
