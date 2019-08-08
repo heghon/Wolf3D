0de69b0a06bbbc632f94436173740f88e9f57ad9
Stan SENECA <sseneca@e1r2p3.42.fr>
2019-08-08 17:13:42 +0200

    make push

--- src/drawing_handler.c
+++ src/drawing_handler.c
@@ -21,7 +21,7 @@ void				pixel_put(t_data *data, int i, unsigned int c)
 	data->mlx.pic[x + 1] = (c >> 8);
 	data->mlx.pic[x + 2] = (c >> 16);
 }
-
+/*
 static void			pixel_put_two(t_data *data, int i, unsigned int c)
 {
 	int		x;
@@ -32,7 +32,7 @@ static void			pixel_put_two(t_data *data, int i, unsigned int c)
 	data->mlx.pic[x + 2] = (float)(c >> 16) / (float)(PROJ_PLANE_H /
 		(WIN_H / 4) - ((float)(i - (WIN_H / 4 * 3)) / (WIN_H / 8)));
 }
-
+*/
 static void			pixel_put_tex(t_data *data, int i, unsigned int t)
 {
 	int d;
@@ -61,9 +61,52 @@ static unsigned int	choose_tex(t_ray *ray)
 	return (0);
 }
 
+static void			pixel_put_floor(t_data *data, int i, unsigned int side)
+{
+	float	floor[2];
+	int		floort[2];
+	int	x;
+	int d;
+
+	x = (i * data->mlx.s_line) + (data->ray.nbr * 4);
+    if(side == 0)//do one time for each line instead of every pixel ?
+    {
+		floor[X] = data->ray.map[X];
+    	floor[Y] = data->ray.map[Y] + data->ray.wall_x;
+    }
+    else if(side == 1)
+    {
+        floor[X] = data->ray.map[X] + 1.0;
+        floor[Y] = data->ray.map[Y] + data->ray.wall_x;
+    }
+    else if(side == 2)
+    {
+        floor[X] = data->ray.map[X] + data->ray.wall_x;
+        floor[Y] = data->ray.map[Y];
+    }
+    else
+    {
+    	floor[X] = data->ray.map[X] + data->ray.wall_x;
+        floor[Y] = data->ray.map[Y] + 1.0;
+    }
+//perpWallDist == data->ray.final_dist
+	float currentdist;
+	float w;
+
+	currentdist = PROJ_PLANE_H / (2.0 * i - PROJ_PLANE_H);
+	w = (currentdist - 0.0) / (data->ray.final_dist - 0.0);
+	floort[X] = (int)((w * floor[X] + (1.0 - w) * data->player.pos[X]) * TEX_S) % TEX_S;// * then % ?//yeah probably
+	floort[Y] = (int)((w * floor[Y] + (1.0 - w) * data->player.pos[Y]) * TEX_S) % TEX_S;
+	d = (floort[Y] * TEX_S + floort[X]) * 4;
+	data->mlx.pic[x] = data->mlx.texpic[3][d];
+	data->mlx.pic[x + 1] = data->mlx.texpic[3][d + 1];
+	data->mlx.pic[x + 2] = data->mlx.texpic[3][d + 2];
+}
+
 void				drawing_handler(int start, int stop, t_data *data)
 {
 	int		i;
+	//int t = choose_tex(), so we don't make the same calcul for every pixel
 
 	i = -1;
 	while (++i < start)
@@ -73,5 +116,6 @@ void				drawing_handler(int start, int stop, t_data *data)
 		pixel_put_tex(data, i, choose_tex(&data->ray));
 	i -= 1;
 	while (++i < PROJ_PLANE_H)
-		pixel_put_two(data, i, data->color.ground_color);
+		pixel_put_floor(data, i, choose_tex(&data->ray));
+		//	pixel_put_two(data, i, data->color.ground_color);
 }
